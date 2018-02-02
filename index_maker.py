2cbe7de562c1376ec1be57c49378b3e501f364c4
Dan Schellenberg <schellenberg@gmail.com>
2018-02-02 15:47:03 -0600

    updated index and index maker

--- index_maker.py
+++ index_maker.py
@@ -7,6 +7,7 @@ INDEX_TEMPLATE = r"""
 <html>
 <body>
 <h2>${header}</h2>
+<p>Examples are sorted chronologically (most recent is at the top)</p>
 <p>
 % for name in names:
     <li><a href="${name}">${name}</a></li>
@@ -16,22 +17,28 @@ INDEX_TEMPLATE = r"""
 </html>
 """
 
-EXCLUDED = ['index.html', 'index_maker.py', '.git', '.gitattributes', 'LICENSE', 'readme.md']
+EXCLUDED = ['index.html', 'index_maker.py', '.git', '.gitattributes', 'LICENSE', 'README.md', 'CNAME']
 
 import os
+import datetime
 import argparse
 
 # May need to do "pip install mako"
 from mako.template import Template
 
+def modification_date(filename):
+    t = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(t)
 
 def main():
-    parser = argparse.ArgumentParser()
+##    only needed if running this from command line...
+##    parser = argparse.ArgumentParser()
 ##    parser.add_argument("directory")
 ##    parser.add_argument("--header")
 ##    args = parser.parse_args()
+    
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    fnames = [fname for fname in sorted(os.listdir(dir_path))
+    fnames = [fname for fname in sorted(os.listdir(dir_path), key=modification_date, reverse=True)
               if fname not in EXCLUDED]
     header = "CS30 Demos"
     with open("index.html", "w") as text_file:
