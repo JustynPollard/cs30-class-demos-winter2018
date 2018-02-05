8032eafc61cd6ac76927a91cf34c68fbba21e368
Dan Schellenberg <schellenberg@gmail.com>
2018-02-05 15:19:12 -0600

    stickman demo

---
+++ superBasicTemplate/script.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+  //body
+  line(200, 200, 200, 400);
+
+  // head of the stickman
+  fill(255, 255, 255);
+  ellipse(200, 200, 100, 100);
+
+  // hat
+  fill(255,0,0);
+  rect(150, 120, 100, 30);
+  rect(175, 70, 50, 50);
+
+  // arms
+  
+
+  // legs
+
+}
