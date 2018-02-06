d963d1f3e45d02f8f20d61d3b11e8df8d56afdbb
Dan Schellenberg <schellenberg@gmail.com>
2018-02-06 14:42:23 -0600

    stickman completed

--- superBasicTemplate/script.js
+++ superBasicTemplate/script.js
@@ -3,22 +3,29 @@ function setup() {
 }
 
 function draw() {
+  background(255);
 
+  drawStickman(mouseX, mouseY);
+  drawStickman(100, 200);
+}
+
+function drawStickman(x, y) {
   //body
-  line(200, 200, 200, 400);
+  line(x, y, x, y+200);
 
   // head of the stickman
   fill(255, 255, 255);
-  ellipse(200, 200, 100, 100);
+  ellipse(x, y, 100, 100);
 
   // hat
-  fill(255,0,0);
-  rect(150, 120, 100, 30);
-  rect(175, 70, 50, 50);
+  fill(255, 0, 0);
+  rect(x-50, y-80, 100, 30);
+  rect(x-25, y-130, 50, 50);
 
   // arms
-  
+  line(x-50, y+100, x+50, y+100);
 
   // legs
-
+  line(x, y+200, x-50, y+250);
+  line(x, y+200, x+50, y+250);
 }
